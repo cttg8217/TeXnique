18176c0797e961e4d62019438fe64569a119ef68
cttg8217 <cttg8217@gmail.com>
2024-03-11 22:04:06 +0900

    timeout to 10 minutes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,5 @@
-let TIMEOUT_SECONDS = 180;
-let TIMEOUT_STRING = "three minutes";
+let TIMEOUT_SECONDS = 600;
+let TIMEOUT_STRING = "ten minutes";
 let secondsRemaining = TIMEOUT_SECONDS;
 
 let gameTimer;
